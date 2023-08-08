785f532d01f3a5f249b016ec66428d6b8a17a3c2
yangyizhou <yizhouyang@yeah.net>
2023-08-08 23:55:34 +0800

    初始化

---
+++ java_concurrent_learning/src/main/java/org/example/test/Test1.java
@@ -0,0 +1,19 @@
+package org.example.test;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j(topic = "c.Test1")
+public class Test1 {
+    public static void main(String[] args) {
+        Thread t = new Thread() {
+            @Override
+            public void run() {
+                log.debug("running");
+            }
+        };
+        t.setName("t1");
+        t.start();
+
+        log.debug("running");
+    }
+}
---
+++ java_concurrent_learning/src/main/java/org/example/test/Test2.java
@@ -0,0 +1,12 @@
+package org.example.test;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j(topic = "c.Test2")
+public class Test2 {
+    public static void main(String[] args) {
+        Runnable runnable = () -> log.debug("running");
+        Thread t2 = new Thread(runnable, "t2");
+        t2.start();
+    }
+}
